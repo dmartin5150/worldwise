6c137cd84890096dc2d6d73a5c5c58a7edc996f8
David Martin <dmartin5150@icloud.com>
2024-05-20 23:41:45 -0500

    created initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import HomePage from './pages/HomePage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+
 function App() {
-  const x = 23;
   return (
-    <div>WorldWise</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
